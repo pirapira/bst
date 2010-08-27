8baa07339e1b65d26876af908e7062161ede86a8
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-08-27 15:07:01 +0900

    now, able to draw graph.

--- Data/SMap/BalanceGraph.hs
+++ Data/SMap/BalanceGraph.hs
@@ -1,4 +1,26 @@
 import Data.SMap.Balance
 
+width :: Int
+width = 600
+height :: Int
+height = 1200
+
+bool_show :: Bool -> String
+bool_show True = "0"
+bool_show False = "1"
+
 main :: IO ()
-main = return ()
+main = do
+    putStrLn "P2" -- portable bitmap
+    putStr (show width)
+    putStr " "
+    putStrLn (show height)
+    putStrLn "1"
+    mapM_ (\y ->
+               do
+                 mapM_ (\x -> (putStr $ (bool_show $ isBalancedSize x (height - 1 - y)) ++ " ")) [0..(width-1)]
+                 putStrLn "")
+   	  [0..(height-1)]
+
+    -- mapM_ (\y -> mapM_ (\x -> (putStrLn $ show x ++ show (height - 1 - y))) [0..(width-1)])
+    -- 	  [0..(height-1)]
